ac94c6ea46b55677afa31a69c4971114a64b3197
woshigerunze <grzaiyxr@icloud.com>
2020-06-16 21:50:17 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <type_traits>
+using namespace std;
+template <typename T,int N>
+struct my_add_pointer
+{
+    using type=typename add_pointer<typename my_add_pointer<T,N-1>::type>::type;
+};
+
+template <typename T>
+struct my_add_pointer<T,0>
+{
+    using type=T;
+};
+
+template <typename ...Args>
+auto sum_fold(Args... args)
+{
+    return (args-...);
+}
+
+int main()
+{
+
+}
