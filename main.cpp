07e4cd65319c6a601e2044e30a0763cb2e5f0054
woshigerunze <grzaiyxr@icloud.com>
2020-06-23 00:06:28 +0800

    boost

--- main.cpp
+++ main.cpp
@@ -2,27 +2,10 @@
 #include <vector>
 #include <type_traits>
 #include <algorithm>
-#include <thread>
+#include <boost/smart_ptr.hpp>
 using namespace std;
 
-template <typename ...Args>
-auto sum_fold(Args... args)
-{
-    return (args+...);
-}
-
-template <typename T>
-typename enable_if<is_same<T,int>::value,T>::type f(T t)
-{
-    cout<<1<<endl;
-}
-template <typename T>
-void g(T t)
-{
-    if constexpr(is_same<T,int>::value)cout<<1<<endl;
-    else cout<<2<<endl;
-}
 int main()
 {
-    g(char());
+    
 }
